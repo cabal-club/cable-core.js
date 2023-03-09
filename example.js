0b9b1ccae15cc42451d38fc76fc04937bc74c3b2
cblgh <cblgh@cblgh.org>
2023-03-09 09:53:50 +0100

    update example.js to contain a standalone mini example of lib
    also showcases an area of improvement in that a mechanism, like done() callbacks, is
    needed for each operation that can be performed and where subsequent
    operations can be reliably performed with confidence that the prior
    operation has finished being processed

--- example.js
+++ example.js
@@ -1,49 +1,19 @@
-const CableCore = require("../index.js").CableCore
-
+const CableCore = require("./index.js").CableCore
 const core = new CableCore()
-// // TODO (2023-02-23): enable synchronization point (allIndexes.ready(cb)) or sync usage of apis
-
-function logem (err, key, res) {
-  console.log("err", err)
-  console.log(key, res)
-}
 
 core.setNick("boop")
 const buf = core.join("testing")
-setTimeout(() => {
-  core.getJoinedChannels((err, data) => {
-    console.log("error?", err)
-    console.log("data?", data) 
-    const hash2 = core.hash(buf)
-    core.del(hash2)
-    setTimeout(() => {
-      core.getJoinedChannels((err, data) => {
-        console.log("2 error?", err)
-        console.log("2 data?", data) 
-        // core.store.reverseMapView.api.getUses(hash2, (err, uses) => {
-        //   console.log("the queried hash (post/join) was", hash2.toString("hex"))
-        //   logem(err, "reverse map uses", uses)
-        //   core.store.reverseMapView.api.del(hash2, () => {
-        //     setTimeout(() => {
-        //       core.store.reverseMapView.api.getUses(hash2, (err, uses) => {
-        //         logem(err, "post del reverse map uses", uses)
-        //       })
-        //     }, 1000)
-        //   })
-        // })
-      })
-    }, 100)
-  })
-}, 200)
-// core.getJoinedChannels((err, channels) => {
-//   logem(err, "joined channels", channels)
-// })
-//
-setTimeout(() => {
-  core.getUsers((err, result) => {
-    logem(err, "get users", result)
-  })
-}, 500)
-// core.getUsersInChannel("introduction", (err, result) => {
-//   logem(err, "get users in channel", result)
-// })
+core.getJoinedChannels((err, channels) => {
+  // should print ["testing"]
+  console.log(channels)
+  const hash = core.hash(buf)
+  // remove join post
+  core.del(hash)
+  // TODO (2023-03-09): introduce callbacks to all actions which will fire when done
+  setTimeout(() => {
+    core.getJoinedChannels((err, channels) => {
+      // should print an empty list
+      console.log(channels)
+    })
+  }, 100)
+})
